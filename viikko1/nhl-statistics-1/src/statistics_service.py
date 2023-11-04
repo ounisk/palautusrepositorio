6743da017c3a1d618e1cbbbb091cf8aaea71fe39
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-04 17:35:11 +0200

    Improve code for unit testing

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,21 +1,15 @@
 from player_reader import PlayerReader
 
-
-def sort_by_points(player):
-    return player.points
-
-
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
-
+    def __init__(self, playerreader_olio): #
+        reader = playerreader_olio  #
         self._players = reader.get_players()
 
     def search(self, name):
         for player in self._players:
             if name in player.name:
                 return player
-
+            
         return None
 
     def team(self, team_name):
@@ -27,6 +21,10 @@ class StatisticsService:
         return list(players_of_team)
 
     def top(self, how_many):
+        # metodin käyttämä apufufunktio voidaan määritellä näin
+        def sort_by_points(player):
+            return player.points
+
         sorted_players = sorted(
             self._players,
             reverse=True,
