4a468d262e0f8b224221a0e072da6554f079234b
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-05 09:53:41 +0200

    Add more tests for sorting

--- viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -1,6 +1,7 @@
 import unittest
-from statistics_service import StatisticsService
+from statistics_service import StatisticsService, SortBy
 from player import Player
+
 
 class PlayerReaderStub:
     def get_players(self):
@@ -30,5 +31,17 @@ class TestStatisticsService(unittest.TestCase):
         self.assertEqual(str(team[0]), "Lemieux PIT 45 + 54 = 99")
 
     def test_sorted_players(self):
-        sorted =self.stats.top(1)
-        self.assertEqual(str(sorted[0]), "Gretzky EDM 35 + 89 = 124") #  "Lemieux PIT 45 + 54 = 99")
+        sorted = self.stats.top(1)
+        self.assertEqual(str(sorted[0]), "Gretzky EDM 35 + 89 = 124") #  top player
+
+    def test_sorted_players_points(self):
+        sorted_points = self.stats.top(4, SortBy.POINTS) 
+        self.assertEqual(str(sorted_points[1]), "Lemieux PIT 45 + 54 = 99") # pisteiden mukaan 2.
+
+    def test_sorted_players_goals(self):
+        sorted_goals = self.stats.top(4, SortBy.GOALS) 
+        self.assertEqual(str(sorted_goals[0]), "Lemieux PIT 45 + 54 = 99") #maalien mukaan 1.
+
+    def test_sorted_players_assists(self):
+        sorted_assists = self.stats.top(4, SortBy.ASSISTS) 
+        self.assertEqual(str(sorted_assists[0]), "Gretzky EDM 35 + 89 = 124") #syöttejen mukaan 1.
