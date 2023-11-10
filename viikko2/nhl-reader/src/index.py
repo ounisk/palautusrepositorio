51ed7055f53e29de31cf108ce33650e731c2a6bc
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-10 16:15:57 +0200

    Remove unnecessary prints

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,8 +5,8 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
 
     players = []
 
