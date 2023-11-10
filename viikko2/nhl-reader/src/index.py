98e849cbd14c1bfd89dfe4f9e766d02b70fa5cdc
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-10 16:13:46 +0200

    Add uncitionality to fetch Finnish players"

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,25 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        if player_dict["nationality"] == "FIN":
+            player = Player(player_dict)
+            players.append(player)
+
+    print("Players from FIN")
+
+    for player in players: #lisää ehto että jos suomalinen niin printtaa - piti hakea suomalisten tiedot...
+        print(player)
+
 
 if __name__ == "__main__":
     main()
