43ef3ec7b1abdeee940737473e9f98ff4ceca54f
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-11-11 19:23:51 +0200

    Refactor code for assignment12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,24 +1,35 @@
 import requests
 from player import Player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    players_all = reader.get_players()       #lista Player-olioita
+   
+    stats = PlayerStats(players_all)
+    players = stats.top_scorers_by_nationality("FIN")  #skreenaus kans. mukaan
 
-    #print("JSON-muotoinen vastaus:")
+    for player in players:
+        print(player)
+    
+    #response = requests.get(url).json()  #Tehtävän 11 vastaus 
+
+    ##print("JSON-muotoinen vastaus:")
     #print(response)
 
-    players = []
+    #players = []
 
-    for player_dict in response:
-        if player_dict["nationality"] == "FIN":
-            player = Player(player_dict)
-            players.append(player)
-    sorted_players = sorted(players, key=lambda x: x.point, reverse = True)
-    print("Players from FIN\n")
+    #for player_dict in response:
+    #    if player_dict["nationality"] == "FIN":
+    #        player = Player(player_dict)
+    #        players.append(player)
+    #sorted_players = sorted(players, key=lambda x: x.point, reverse = True)
+    #print("Players from FIN\n")
     
-    for player in sorted_players: #lisää ehto että jos suomalinen niin printtaa - piti hakea suomalisten tiedot...
-        print(player)
+    #for player in sorted_players: #lisää ehto että jos suomalinen niin printtaa - piti hakea suomalisten tiedot...
+    #    print(player)
 
 
 if __name__ == "__main__":
