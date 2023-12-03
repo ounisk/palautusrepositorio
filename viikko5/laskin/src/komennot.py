6f8870c82ce9c7553a2969bcf085363326fe44f1
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-12-03 10:35:08 +0200

    Add "kumoa" functionality to classes

--- viikko5/laskin/src/komennot.py
+++ viikko5/laskin/src/komennot.py
@@ -2,28 +2,36 @@ from sovelluslogiikka import Sovelluslogiikka
 # Huom! ohje : mikään näistä EI palauta mitään!!!
 
 class Summa:
-    def __init__(self, sovelluslogiikka, syote):#, luku1): #, luku1):
+    def __init__(self, sovelluslogiikka, syote): #, vanha_tulos):
         #super().__init__(io)
         #self.io = io
         #self.luku1 = luku1
         self._sovelluslogiikka = sovelluslogiikka
         self._syote = syote
+        self.vanha_tulos = 0
+        #self._vanha_tulos = syote[1]
 
     def suorita(self): #, arvo):
-        #self.luku1 = int(self.io)
-        #luku1 = int(self.io.read())
-        #luku2 = int(self.io.read())
-        #return arvo+ self.luku1 #+ self.luku2 #
+        self.vanha_tulos = self._sovelluslogiikka.arvo()
         self._sovelluslogiikka.plus(self._syote())   ### tämä piti olla
+
+    def kumoa(self): #, syote):
+        self._sovelluslogiikka.aseta_arvo(self.vanha_tulos)
+
         
 class Erotus:
     def __init__(self, sovelluslogiikka, syote):
         #super().__init__(io)
         self._sovelluslogiikka = sovelluslogiikka
         self._syote = syote
+        self.vanha_tulos = 0
 
     def suorita(self):
+        self.vanha_tulos = self._sovelluslogiikka.arvo()
         self._sovelluslogiikka.miinus(self._syote())
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self.vanha_tulos)    
     
 
 class Nollaus:   # ei saa syötettä
@@ -31,8 +39,13 @@ class Nollaus:   # ei saa syötettä
         #super().__init__(io)
         self._sovelluslogiikka = sovelluslogiikka
         self._syote = syote
+        self.vanha_tulos = 0
 
     def suorita(self):
         #return 0
         #self._sovelluslogiikka.nollaa(self._syote())
+        self.vanha_tulos = self._sovelluslogiikka.arvo()
         self._sovelluslogiikka.nollaa()
+
+    def kumoa(self):
+        self._sovelluslogiikka.aseta_arvo(self.vanha_tulos)     
