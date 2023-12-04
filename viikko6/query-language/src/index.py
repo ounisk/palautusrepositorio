bed5df05fd7b497fe4e97a5fc863b85c58c2bfc8
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-12-04 10:27:19 +0200

    Add QueryBuilder for assignm.4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,48 +1,66 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or, QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
-    )
+#Tehtävä 4:
+    query = QueryBuilder()
+    #matcher = query.build()                        #1.tapaus: All, number = 1058
+    #matcher = query.playsIn("NYR").build()          #2. tapaus: plays in "NYR" = 22
+    matcher = (                                      #3.tapaus  - And mukaan                                 
+        query
+        .playsIn("NYR")
+        .hasAtLeast(10, "goals")
+        .hasFewerThan(20, "goals")
+        .build()
+        )
 
     for player in stats.matches(matcher):
         print(player)
 
-    filtered_with_all = stats.matches(All())
-    print(len(filtered_with_all))
+    print("Yllä teht 4 tulostus.")
+
+# Tehtävä 3:
+    #matcher = And(
+    #    HasAtLeast(5, "goals"),
+    #    HasAtLeast(20, "assists"),
+    #    PlaysIn("PHI")
+    #)
+
+    #for player in stats.matches(matcher):
+    #    print(player)
+    #print("All")
+    #filtered_with_all = stats.matches(All())
+    #print(len(filtered_with_all))
 
     matcher = And(
         Not(HasAtLeast(2, "goals")),
         PlaysIn("NYR")
     )
-    for player in stats.matches(matcher):
-        print(player)
+    #for player in stats.matches(matcher):
+    #    print(player)
 
-    print("tulostaa saman kuin yo.")
+    #print("tulostaa saman kuin yo.")
 
     matcher = And(
         HasFewerThan(2, "goals"),
         PlaysIn("NYR")
     )
 
-    for player in stats.matches(matcher):
-        print(player)
+    #for player in stats.matches(matcher):
+    #    print(player)
 
     matcher = Or(
         HasAtLeast(45, "goals"),
         HasAtLeast(70, "assists")
     )
-    print("**Or -testaus**")
-    for player in stats.matches(matcher):
-        print(player)
+    #print("**Or -testaus**")
+    #for player in stats.matches(matcher):
+    #    print(player)
 
 
 
@@ -54,10 +72,11 @@ def main():
             PlaysIn("BOS")
         )
     )
-    
-    print("**ISO Or -testaus**")
-    for player in stats.matches(matcher):
-        print(player)
+
+    #print("**ISO Or -testaus**")
+    #for player in stats.matches(matcher):
+    #    print(player)
+
 
 
 
