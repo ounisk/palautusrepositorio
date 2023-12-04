2444e475662ab1f8939a50cfa004c2e5533fab38
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-12-04 10:27:55 +0200

    Add class QueryBuilder assignm4

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,3 +1,5 @@
+from player_reader import PlayerReader
+
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
@@ -30,10 +32,9 @@ class HasAtLeast:
 
 
 class All:
-    
-    @staticmethod
+    @staticmethod  
     def test(player):
-        return True 
+        return True
 
 
 class Not:
@@ -65,7 +66,33 @@ class Or:
     def test(self, player):
         for matcher in self._matchers:
         #while matcher in self._matchers:
-            if matcher.test(player): #vai if not....
+            if matcher.test(player):
                 return True
         
         return False    
+    
+class QueryBuilder:    #tehtävä 4
+    #def __init__(self):   #1. tapaus, jossa vain All
+    #    self.query_object = All()
+
+    def __init__(self, query = All()):   #2.tapaus, jossa playsIn "NYR"
+        self.query_object = query
+
+    #def __init__(self, query = And()):   #3 tapaus, jossa ketjutettuja ehtoja / ei tätät
+    #    self.query_object = query
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self.query_object,PlaysIn(team)))   #kohtaan #3 yhdistetään hakuja.
+        #return QueryBuilder(PlaysIn(team))  # kohtaan #2
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self.query_object, HasAtLeast(value,attr))) 
+        #return QueryBuilder(HasAtLeast(value,attr))    # draft versio 
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self.query_object, HasFewerThan(value, attr)))
+        #return QueryBuilder(HasFewerThan(value,attr))   #draft versio
+
+
+    def build(self):
+        return self.query_object
