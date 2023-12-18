0d3f1e2506823fbb01e6b5c95278fe5a6a77d38d
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-12-18 13:57:07 +0200

    Refactor for w7 assignm.3&4

--- viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
+++ viikko7/kivi-paperi-sakset/src/kps_pelaaja_vs_pelaaja.py
@@ -1,22 +1,14 @@
 from tuomari import Tuomari
+from kivi_paperi_sakset import KiviPaperiSakset
 
 
-class KPSPelaajaVsPelaaja:
-    def pelaa(self):
-        tuomari = Tuomari()
+class KPSPelaajaVsPelaaja(KiviPaperiSakset):
+    #def pelaa(self):
+    #    tuomari = Tuomari()
+        #ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+    #@staticmethod
+
+    def toisen_siirto(self, ensimmäisen_siirto):
         tokan_siirto = input("Toisen pelaajan siirto: ")
-
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
-
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = input("Toisen pelaajan siirto: ")
-
-        print("Kiitos!")
-        print(tuomari)
-
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+        return tokan_siirto
