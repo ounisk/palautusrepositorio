347606605f573c2314f8dbf01ff63d362b1e56d9
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-12-18 13:54:27 +0200

    initial commit for pelitehdas.py

---
+++ viikko7/kivi-paperi-sakset/src/pelitehdas.py
@@ -0,0 +1,17 @@
+from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
+from kps_tekoaly import KPSTekoaly
+from kps_parempi_tekoaly import KPSParempiTekoaly
+
+
+class Pelitehdas:
+    @staticmethod
+    def luo_kaksinpeli():
+        return KPSPelaajaVsPelaaja()
+    
+    @staticmethod
+    def luo_yksinpeli():
+        return KPSTekoaly()
+
+    @staticmethod   
+    def luo_vaikea_yksinpeli():
+        return KPSParempiTekoaly()
