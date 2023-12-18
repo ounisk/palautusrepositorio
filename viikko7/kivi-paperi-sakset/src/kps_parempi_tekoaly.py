9e9081b5e876697591b17c813193aae2f9cbb00a
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-12-18 13:56:32 +0200

    Refactor for w7 assignment 3‚4

--- viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
+++ viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
@@ -1,29 +1,40 @@
 from tuomari import Tuomari
 from tekoaly_parannettu import TekoalyParannettu
+from kivi_paperi_sakset import KiviPaperiSakset
 
 
-class KPSParempiTekoaly:
-    def pelaa(self):
-        tuomari = Tuomari()
-        tekoaly = TekoalyParannettu(10)
+class KPSParempiTekoaly(KiviPaperiSakset):
+    def __init__(self): #, tekoaly_parannettu):
+        self._tekoaly_parannettu = TekoalyParannettu(10)
 
-        ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-        tokan_siirto = tekoaly.anna_siirto()
-
+    def toisen_siirto(self, ensimmäisen_siirto):
+        tokan_siirto = self._tekoaly_parannettu.anna_siirto()
         print(f"Tietokone valitsi: {tokan_siirto}")
+        self._tekoaly_parannettu.aseta_siirto(ensimmäisen_siirto)
+        return tokan_siirto    
+    
 
-        while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
-            tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
-            print(tuomari)
+    #def pelaa(self):
+    #    tuomari = Tuomari()
+    #    tekoaly = TekoalyParannettu(10)
 
-            ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
-            tokan_siirto = tekoaly.anna_siirto()
+        #ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+        #tokan_siirto = tekoaly.anna_siirto()
 
-            print(f"Tietokone valitsi: {tokan_siirto}")
-            tekoaly.aseta_siirto(ekan_siirto)
+        #print(f"Tietokone valitsi: {tokan_siirto}")
 
-        print("Kiitos!")
-        print(tuomari)
+        #while self._onko_ok_siirto(ekan_siirto) and self._onko_ok_siirto(tokan_siirto):
+        #    tuomari.kirjaa_siirto(ekan_siirto, tokan_siirto)
+        #    print(tuomari)
 
-    def _onko_ok_siirto(self, siirto):
-        return siirto == "k" or siirto == "p" or siirto == "s"
+        #    ekan_siirto = input("Ensimmäisen pelaajan siirto: ")
+        #    tokan_siirto = tekoaly.anna_siirto()
+
+        #    print(f"Tietokone valitsi: {tokan_siirto}")
+        #    tekoaly.aseta_siirto(ekan_siirto)
+
+        #print("Kiitos!")
+        #print(tuomari)
+
+    #def _onko_ok_siirto(self, siirto):
+    #    return siirto == "k" or siirto == "p" or siirto == "s"
