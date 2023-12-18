19465c0274bd7ba7c882fb7685e738087e31eead
Outi Nissinen <78747844+ounisk@users.noreply.github.com>
2023-12-18 13:53:53 +0200

    Refactor index.py

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,9 +1,16 @@
 from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
 from kps_tekoaly import KPSTekoaly
 from kps_parempi_tekoaly import KPSParempiTekoaly
+from pelitehdas import Pelitehdas
 
 
 def main():
+
+    pelit = {"a": Pelitehdas.luo_kaksinpeli(),
+             "b": Pelitehdas.luo_yksinpeli(),
+             "c": Pelitehdas.luo_vaikea_yksinpeli()
+             }
+
     while True:
         print("Valitse pelataanko"
               "\n (a) Ihmistä vastaan"
@@ -14,27 +21,30 @@ def main():
 
         vastaus = input()
 
-        if vastaus.endswith("a"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
+        #if vastaus.endswith("a"):
+        #    print(
+        #        "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+        #    )
 
-            kaksinpeli = KPSPelaajaVsPelaaja()
-            kaksinpeli.pelaa()
-        elif vastaus.endswith("b"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
+        #    kaksinpeli = KPSPelaajaVsPelaaja()
+        #    kaksinpeli.pelaa()
+        #elif vastaus.endswith("b"):
+        #    print(
+        #        "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+        #    )
 
-            yksinpeli = KPSTekoaly()
-            yksinpeli.pelaa()
-        elif vastaus.endswith("c"):
-            print(
-                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
-            )
+        #    yksinpeli = KPSTekoaly()
+        #    yksinpeli.pelaa()
+        #elif vastaus.endswith("c"):
+        #    print(
+        #        "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+        #    )
 
-            haastava_yksinpeli = KPSParempiTekoaly()
-            haastava_yksinpeli.pelaa()
+        #    haastava_yksinpeli = KPSParempiTekoaly()
+        #    haastava_yksinpeli.pelaa()
+        if vastaus in pelit:
+            pelit[vastaus].pelaa()
+            print("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
         else:
             break
 
